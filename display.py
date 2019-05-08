98ce30c55b36e48e52540b9e4b268427f608605e
Jim van Leeuwen <jim.vanleeuwen@student.hu.nl>
2019-05-08 20:36:49 +0200

    display class cam show two images side by side

--- display.py
+++ display.py
@@ -1,4 +1,4 @@
-from tkinter import *
+import tkinter as tk
 from PIL import Image, ImageTk
 
 class Display:
@@ -10,13 +10,30 @@ class Display:
     prevents direct blocking from the tkinter main loop.
     '''
     def __init__(self, image_width: int, image_height: int):
-        self.window = Tk()
+        self.image_width = image_width
+        self.image_height = image_height
 
-        empty_image = Image.new('RGB', (image_width, image_height), (0, 0, 0))
-        self.displayable_image = ImageTk.PhotoImage(empty_image)
+        self.window = tk.Tk()
+        self.window.geometry("{}x{}".format(image_width*2, image_height))
 
-        self.panel = Label(self.window, image=self.displayable_image)
-        self.panel.pack(side="bottom", fill="both", expand="yes")
+        self.frame = tk.Frame(self.window)
+        self.frame.pack(fill=tk.BOTH, expand=True)
+        self.canvas = tk.Canvas(self.frame)
+        self.canvas.pack(fill=tk.BOTH, expand=True)
+
+
+        empty_image1 = Image.new('RGB', (image_width, image_height), (0, 0, 0))
+        self.displayable_image1 = ImageTk.PhotoImage(empty_image1)
+
+        empty_image2 = Image.new('RGB', (image_width, image_height), (0, 0, 0))
+        self.displayable_image2 = ImageTk.PhotoImage(empty_image2)
+
+        self.frame.image1 = self.canvas.create_image((image_width/2, image_height/2), image=self.displayable_image1)
+        self.frame.image2 = self.canvas.create_image((image_width/2, image_height/2), image=self.displayable_image2)
+        self.canvas.move(self.frame.image2, image_width, 0)
+
+        self.frame.pack(fill=tk.BOTH, expand=True)
+
 
     '''
     Starts the window loop, this function is blocking for anything that comes after this function until the window
@@ -28,8 +45,22 @@ class Display:
     '''
     This function allows displayed images to be changed
     '''
-    def changeImage(self, new_image: Image):
-        self.displayable_image = ImageTk.PhotoImage(new_image)
-        self.panel.configure(image=self.displayable_image)
-        self.panel.image=self.displayable_image
+    def changeImage1(self, new_image: Image):
+        self.displayable_image1 = ImageTk.PhotoImage(new_image)
+        self.image1 = new_image
+        self.frame.image1 = self.canvas.create_image((self.image_width/2, self.image_height/2), image=self.displayable_image1)
+
+        #self.displayable_image = ImageTk.PhotoImage(new_image)
+        #self.panel.configure(image=self.displayable_image)
+        #self.panel.image=self.displayable_image
+
+
+    '''
+    This function allows displayed images to be changed
+    '''
+    def changeImage2(self, new_image: Image):
+        self.displayable_image2 = ImageTk.PhotoImage(new_image)
+        self.image2 = new_image
+        self.frame.image2 = self.canvas.create_image((self.image_width/2, self.image_height/2), image=self.displayable_image2)
+        self.canvas.move(self.frame.image2, self.image_width, 0)
 
--- main.py
+++ main.py
@@ -1,7 +1,30 @@
 import display
+import time
+import threading
+from PIL import Image
+
+def swapper():
+    global window
+    imageIndex = False
+    image1 = Image.open("D:\\Images\\testImage.png")
+    image2 = Image.open("D:\\Images\\testImage2.png")
+    window.changeImage2(image1)
+
+    while (True):
+        if (imageIndex):
+            window.changeImage1(image1)
+            imageIndex = False
+        else:
+            window.changeImage1(image2)
+            imageIndex = True
+        time.sleep(1)
+
+
 
 window = display.Display(300, 291)
 
 #Create workers that work from separate thread
+worker1 = threading.Thread(target=swapper)
+worker1.start()
 
 window.startWindowLoop()
