da21d4b50cc271f68489d2c19bc9f09021f78a28
Tonyvdk <t.r.vanderkrogt@gmail.com>
2019-05-10 15:52:28 +0200

    Get all RGB values of a snapshot

--- main.py
+++ main.py
@@ -14,29 +14,33 @@ def choose_camera(camera):
     return cap
 
 
-def snap(cap):
-    (ret, frame) = cap.read()
-    snapshot = np.zeros(frame.shape, dtype=np.uint8)
-    cv2.imshow('Snapshot', snapshot)
-
-    color_array = np.zeros((80, 250, 3), dtype=np.uint8)
-    cv2.imshow('Color', color_array)
-
-
 def commands(cap, frame):
     if cv2.waitKey(1) & 0xFF == ord('q'):
         exit_cam(cap)
     elif cv2.waitKey(1) & 0xFF == ord('s'):
         snapshot = frame.copy()
         cv2.imshow('Snapshot', snapshot)
+        get_pixel_values(snapshot)
+
+
+def get_pixel_values(snap):
+    height = snap.shape[0]
+    width = snap.shape[1]
+    pixel_values = [[[0 for n in range(3)] for row in range(width)] for col in range(height)]
+    for y in range(height):
+        for x in range(width):
+            pixel_values[y][x][0] = snap[y, x, 0]
+            pixel_values[y][x][1] = snap[y, x, 1]
+            pixel_values[y][x][2] = snap[y, x, 2]
+    return pixel_values
 
 
 def main():
-    camera = choose_camera(1)
+    cam = choose_camera(1)
     while(True):
-        (ret, frame) = camera.read()
+        (ret, frame) = cam.read()
         cv2.imshow('Frame', frame)
-        commands(camera, frame)
+        commands(cam, frame)
 
 
 main()
