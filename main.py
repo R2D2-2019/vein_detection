9e777152240aa76671a75f506a435f026003af2f
Tonyvdk <t.r.vanderkrogt@gmail.com>
2019-05-20 11:58:05 +0200

    code is in a class now. Changed function names to appropriate code style

--- main.py
+++ main.py
@@ -2,45 +2,48 @@ import cv2
 import numpy as np
 
 
-def exit_cam(cap):
-    cap.release()
-    cv2.destroyAllWindows()
+class camera_c():
+
+    def exit_cam(self, cap):
+        cap.release()
+        cv2.destroyAllWindows()
 
 
-def choose_camera(camera):
-    cap = cv2.VideoCapture(camera)
-    if not cap.isOpened():
-        raise RuntimeError('Error opening camera.')
-    return cap
+    def choose_camera(self, camera):
+        cap = cv2.VideoCapture(camera)
+        if not cap.isOpened():
+            raise RuntimeError('Error opening camera.')
+        return cap
 
 
-def commands(cap, frame):
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        exit_cam(cap)
-    elif cv2.waitKey(1) & 0xFF == ord('s'):
-        snapshot = frame.copy()
-        cv2.imshow('Snapshot', snapshot)
-        get_pixel_values(snapshot)
+    def commands(self, cap, frame):
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            self.exit_cam(cap)
+        elif cv2.waitKey(1) & 0xFF == ord('s'):
+            snapshot = frame.copy()
+            cv2.imshow('Snapshot', snapshot)
+            self.get_pixel_values(snapshot)
 
 
-def get_pixel_values(snap):
-    height = snap.shape[0]
-    width = snap.shape[1]
-    pixel_values = [[[0 for n in range(3)] for row in range(width)] for col in range(height)]
-    for y in range(height):
-        for x in range(width):
-            pixel_values[y][x][0] = snap[y, x, 0]
-            pixel_values[y][x][1] = snap[y, x, 1]
-            pixel_values[y][x][2] = snap[y, x, 2]
-    return pixel_values
+    def get_pixel_values(self, snap):
+        height = snap.shape[0]
+        width = snap.shape[1]
+        pixel_values = [[[0 for n in range(3)] for row in range(width)] for col in range(height)]
+        for y in range(height):
+            for x in range(width):
+                pixel_values[y][x][0] = snap[y, x, 0]
+                pixel_values[y][x][1] = snap[y, x, 1]
+                pixel_values[y][x][2] = snap[y, x, 2]
+        return pixel_values
 
 
 def main():
-    cam = choose_camera(1)
-    while(True):
+    camera = camera_c()
+    cam = camera.choose_camera(1)
+    while (True):
         (ret, frame) = cam.read()
         cv2.imshow('Frame', frame)
-        commands(cam, frame)
+        camera.commands(cam, frame)
 
 
 main()
