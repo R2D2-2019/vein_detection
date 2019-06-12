a88f07a371e0d1a771f152a89632920239f75ed9
YouriOkkerse <youri.okkerse@student.hu.nl>
2019-06-12 18:08:32 +0200

    Moved commands from camera class to vein_detection class. Minor change in main to use commands from vein detection class. Added CLAHE function too vein_detection. This function provides improved contrast for the image given by the IR-camera.

--- main.py
+++ main.py
@@ -1,16 +1,19 @@
 import camera_handler as camera
+import vein_detection as vd
 import cv2
 import time
 
 
 def main():
     cam_class = camera.CameraHandler(0)
+    vd_class = vd.VeinDetection(0)
+
     # wait for camera to warm up
     time.sleep(1)
     while True:
         (ret, frame) = cam_class.camera.read()
         cv2.imshow('Frame', frame)
-        cam_class.commands(frame)
+        vd_class.commands(frame)
 
 
 if __name__ == "__main__":
--- vein_detection.py
+++ vein_detection.py
@@ -1,5 +1,7 @@
 import cv2
+import numpy as np
 import camera_handler as camera
+
 
 # Main Vein Detection class
 class VeinDetection:
@@ -16,10 +18,19 @@ class VeinDetection:
     # CLAHE (Contrast Limited Adaptive Histogram Equalization)
     # creates a better constrast between veins and the skin on supplied frame
     # returns the resulted frame
+    # The frame (image) is devided into small blocks 8x8 pixels by default
+    # Each of these blocks are then histogram equalized as usual
+    # Contrast limiting is applied to avoid noise amplification
     # NOTE: This function should be made private (since these functions are going to get called inside run())
     # it's public for testing purposes now.
     def clahe(self, frame):
-        return frame
+        input_frame_grayscaled = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+        # create a CLAHE object
+        clahe_object = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8, 8))
+        output_frame = clahe_object.apply(input_frame_grayscaled)
+        result = np.hstack((input_frame_grayscaled, output_frame))
+        return result
 
     # Image Denoising is used to remove noice from supplied frame
     # returns frame with less noise
@@ -34,6 +45,17 @@ class VeinDetection:
     # it's public for testing purposes now.
     def adaptive_thresholding(self, frame):
         return frame
+
+    # this function provides the user with keyboard commands / actions
+    # press 's' to call show_current_frame(frame)
+    # press 'q' to call exit_camera()
+    def commands(self, frame):
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            self.__camera.exit_camera()
+        elif cv2.waitKey(1) & 0xFF == ord('s'):
+            self.__camera.show_current_frame(frame)
+        elif cv2.waitKey(1) & 0xFF == ord('v'):
+            cv2.imshow('Vein Detection', self.clahe(frame))
 
     # This is the main loop for vein detection.
     # Run this after calling the constructor
