2697d4faf6740358c011aa15336a383d39168c14
Deniz <deniz.lektemur@student.hu.nl>
2019-06-26 13:16:24 +0200

    Added commented out code back in

--- test/test_image_denoising.py
+++ test/test_image_denoising.py
@@ -1,8 +1,9 @@
 """this module tests the hsv class"""
 import sys
 import os
-# ROOT_DIR = os.path.abspath(os.curdir)
-# sys.path.append(ROOT_DIR)
+
+ROOT_DIR = os.path.abspath(os.curdir)
+sys.path.append(ROOT_DIR)
 import module.vein_detection
 import cv2
 import numpy as np
@@ -23,11 +24,13 @@ image_denoised = vein_detection_class.image_denoising(base_image)
 histo_test_denoised = cv2.calcHist([test_image_denoised], [0], None, [256], [0, 256])
 histo_denoised = cv2.calcHist([image_denoised], [0], None, [256], [0, 256])
 
+
 def test_height():
     """this test asserts that the height of both images are equal"""
     test_shape = test_image_denoised.shape
     shape = image_denoised.shape
     assert test_shape[0] == shape[0]
+
 
 def test_width():
     """this test asserts that the width of both images are equal"""
@@ -35,18 +38,20 @@ def test_width():
     shape = image_denoised.shape
     assert test_shape[1] == shape[1]
 
+
 def test_channels():
     """this test asserts that the hsv channels of both images are equal"""
     test_shape = test_image_denoised.shape
     shape = image_denoised.shape
     assert test_shape[2] == shape[2]
 
+
 def test_histogram_length():
     """this test asserts that the histogram sizes of both images are equal"""
     assert len(histo_test_denoised) == len(histo_denoised)
+
 
 def test_histogram_values():
     """this test asserts that the histogram values of both images are equal"""
     for gray_value in range(len(histo_denoised)):
         assert histo_test_denoised[gray_value] == histo_denoised[gray_value]
-
